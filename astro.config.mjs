1272c47aa6606b57072fac986f2e7d69008a48a3
Wanty Jean-François <wanty.jf@gmail.com>
2024-03-13 17:14:51 +0100

    Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://golden-jelly-4a3dd7.netlify.app"
+  site: "https://golden-jelly-4a3dd7.netlify.app",
+  integrations: [preact()]
 });

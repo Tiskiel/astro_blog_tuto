e259e30f29b44868366aa13bd180bd52c556a763
Wanty Jean-François <wanty.jf@gmail.com>
2024-03-13 17:12:57 +0100

    Implementing rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://golden-jelly-4a3dd7.netlify.app"
+});
